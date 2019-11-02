4ca6aba62b213faa2c6014bf3c309730b92c6e15
shebna12 <shebna12@gmail.com>
2019-11-02 15:41:57 +0800

    added QR decomposition

---
+++ qr-factorization.py
@@ -0,0 +1,40 @@
+import numpy as np
+import math
+import fractions
+np.set_printoptions(formatter={'all':lambda x: str(fractions.Fraction(x).limit_denominator())})
+
+# A = np.array([[2,-2,18],[2,1,0],[1,2,0]],dtype=float)
+# A = np.array([[1,2,4],[0,0,5],[0,3,6]],dtype=float)
+# A = np.array([[2,1],[2,1],[1,5]],dtype=float)
+A = np.array([[-1,-1,1],[1,3,3],[-1,-1,5],[1,3,7]],dtype=float)
+
+
+print("A \n",A)
+V = np.transpose(A)
+print("V \n",V)
+m = np.shape(V)[0]
+n = np.shape(V)[1]
+Q = np.zeros((m,n),dtype=float)
+Q[0] = V[0]
+
+for col_n in range(1,m):
+	summation = 0
+	for u_idx in range(col_n-1,-1,-1):
+		summation = summation - ((np.dot(Q[u_idx],V[col_n])/np.dot(Q[u_idx],Q[u_idx]))*Q[u_idx]) 
+		# print("((Q{} * V{}) / Q{} * Q{})*Q{} => {}".format(u_idx+1,col_n+1,u_idx+1,u_idx+1,u_idx+1,summation))
+	Q[col_n] = V[col_n] + summation
+	print("Q{}: {}".format(col_n+1,Q[col_n]))
+print("Orthogonalized Vectors")
+print(np.transpose(Q))
+
+
+# Normalize
+for col_n in range(0,m):
+	Q[col_n] = (1/math.sqrt(np.sum(Q[col_n]**2))) * Q[col_n]
+print("Q")
+print(np.transpose(Q))
+
+# R = Q^T A
+R = np.dot(Q,A)
+print("R")
+print(R)
