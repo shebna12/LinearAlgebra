7aabf8a51134730d308f0e8b52b2ae4ad2cce585
shebna <shebna12@gmail.com>
2019-11-08 13:42:33 +0800

    modified hilbert

--- hilbert.py
+++ hilbert.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
 import fractions
+from cholesky import cholesky
 # np.set_printoptions(formatter={'all':lambda x: str(fractions.Fraction(x).limit_denominator())})
 
-n = 5
+n = 12
 x = np.ones((n,1))
-A = np.zeros((n,n),dtype=np.float)
+A = np.zeros((n,n),dtype=np.float64)
 for i in range(1,n+1):
 	for j in range(1,n+1):
 		A[i-1][j-1] = 1/(i+j-1)
@@ -17,4 +18,27 @@ print(A)
 # print(x.T)
 b = np.dot(A,x)
 print("*******************b*********************")
-print(b)
+print(b.T)
+
+x_prime = cholesky(A,b.T)
+# A_lol = np.concatenate((A,b),axis=1)
+# print("A_lol")
+# print(A_lol)
+x_better_prime = np.linalg.cholesky(A)
+print("X Prime")
+print(x_prime)
+print("X Better Prime")
+print(x_better_prime)
+
+#### Check error
+# Norm
+# This is ok. Already checked this.
+euclidean_norm = math.sqrt(np.sum((x.T-x_prime)**2))
+
+# errors_list = [abs(x[idx] - x_prime[idx]) for idx in range(0,n)]
+
+r = b.T - np.dot(A,x_prime)
+print("Residual")
+print(r)
+print("Euclidean Norm")
+print(euclidean_norm)	
